6f84324cd8e72ced5fca3d5d7285e84bc7d9fb77
Brendan Beck <brendan2040@live.missouristate.edu>
2021-10-01 20:26:01 +0100

    added sound processing, retrained model w processed set

--- external_programs/utils.py
+++ external_programs/utils.py
@@ -3,6 +3,9 @@ import numpy as np
 import librosa
 import glob
 import os
+import noisereduce
+from scipy import signal as sg
+
 from sklearn.model_selection import train_test_split
 
 AVAILABLE_EMOTIONS = {
@@ -14,6 +17,49 @@ AVAILABLE_EMOTIONS = {
 }
 
 
+# Using scipy's butterworth filter to highpass frequencies
+def highPassFilterStage(signal, sampleRate, cutoff=120.):
+    b, a = sg.butter(6, cutoff / (sampleRate / 2.), 'highpass', analog=False)
+    filteredSignal = sg.filtfilt(b, a, signal)
+    return filteredSignal
+
+# Using denoise library to denoise the signal
+def denoiseStage(signal, sampleRate):
+    # time_constant_s is the time (sec) to compute the noise floor, increased from 1 to 4 for better results
+    return noisereduce.reduce_noise(y= signal, sr = sampleRate, stationary=False, time_constant_s=4.0)
+
+# Use librosa to cut off silent sections before and after the voice command
+def trimStage(signal, samplePad=10000, threshold = 25):
+    trimmedSignal, index = librosa.effects.trim(signal, top_db=threshold, frame_length=256, hop_length=64)
+    # Add constant padding of samples before and after the non-silent indexes
+    beginningPadCheck = index[0] - samplePad
+    endingPadCheck = index[1] + samplePad
+    numSamples = len(signal) - 1
+
+    # Ensure that padding does not exceed the buffer
+    # We cannot have a negative starting index, so make it zero. If its positive, keep it as it is.
+    if (beginningPadCheck < 0):
+        beginningIndex = 0
+    else:
+        beginningIndex = beginningPadCheck
+
+    # We cannot have an ending index greater than the length of the buffer; In this case, return the last index of
+    # the buffer.
+    if (endingPadCheck > numSamples):
+        endingIndex = numSamples
+    else:
+        endingIndex = endingPadCheck
+
+    # Return the trimmed signal with padding to prevent slight cutoff in commands
+    return signal[beginningIndex:endingIndex]
+
+# Use this function for processing if you already have the signal loaded using librosa
+def processPreloadedAudio(inputSignal, inputSignalSampleRate):
+    highPassedInputSignal = highPassFilterStage(inputSignal, inputSignalSampleRate)
+    denoisedSignal = denoiseStage(highPassedInputSignal, inputSignalSampleRate)
+    processedSignal = trimStage(denoisedSignal)
+    return processedSignal, inputSignalSampleRate
+
 def extract_feature(file_name, **kwargs):
 
     mfcc = kwargs.get("mfcc")
@@ -21,32 +67,36 @@ def extract_feature(file_name, **kwargs):
     mel = kwargs.get("mel")
     contrast = kwargs.get("contrast")
     tonnetz = kwargs.get("tonnetz")
-    with soundfile.SoundFile(file_name) as sound_file:
-        X = sound_file.read(dtype="float32")
-        sample_rate = sound_file.samplerate
-        if chroma or contrast:
-            stft = np.abs(librosa.stft(X))
-        result = np.array([])
-        if mfcc:
-            mfccs = np.mean(librosa.feature.mfcc(
-                y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
-            result = np.hstack((result, mfccs))
-        if chroma:
-            chroma = np.mean(librosa.feature.chroma_stft(
-                S=stft, sr=sample_rate).T, axis=0)
-            result = np.hstack((result, chroma))
-        if mel:
-            mel = np.mean(librosa.feature.melspectrogram(
-                X, sr=sample_rate).T, axis=0)
-            result = np.hstack((result, mel))
-        if contrast:
-            contrast = np.mean(librosa.feature.spectral_contrast(
-                S=stft, sr=sample_rate).T, axis=0)
-            result = np.hstack((result, contrast))
-        if tonnetz:
-            tonnetz = np.mean(librosa.feature.tonnetz(
-                y=librosa.effects.harmonic(X), sr=sample_rate).T, axis=0)
-            result = np.hstack((result, tonnetz))
+
+    inputSignal, inputSignalSampleRate = librosa.load(file_name, sr=None)
+
+    #audio processing stages invoked here
+    X, sample_rate = processPreloadedAudio(inputSignal, inputSignalSampleRate)
+
+    if chroma or contrast:
+        stft = np.abs(librosa.stft(X))
+    result = np.array([])
+    if mfcc:
+        mfccs = np.mean(librosa.feature.mfcc(
+            y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+        result = np.hstack((result, mfccs))
+    if chroma:
+        chroma = np.mean(librosa.feature.chroma_stft(
+            S=stft, sr=sample_rate).T, axis=0)
+        result = np.hstack((result, chroma))
+    if mel:
+        mel = np.mean(librosa.feature.melspectrogram(
+            X, sr=sample_rate).T, axis=0)
+        result = np.hstack((result, mel))
+    if contrast:
+        contrast = np.mean(librosa.feature.spectral_contrast(
+            S=stft, sr=sample_rate).T, axis=0)
+        result = np.hstack((result, contrast))
+    if tonnetz:
+        tonnetz = np.mean(librosa.feature.tonnetz(
+            y=librosa.effects.harmonic(X), sr=sample_rate).T, axis=0)
+        result = np.hstack((result, tonnetz))
+
     return result
 
 
